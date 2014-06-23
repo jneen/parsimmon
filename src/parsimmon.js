4fa7c9569a432337092c3a4aa3c968dd4c7b00ff
Sebastien Armand - sa250111 <sebastien.armand@crm-factory.com.cn>
2014-06-23 18:55:32 +0800

    Allow custom primitive parsers
    Adds `Parsimmon.custom` to allow the definition of primitive custom parsers.

--- src/parsimmon.js
+++ src/parsimmon.js
@@ -97,6 +97,15 @@ Parsimmon.Parser = P(function(_, _super, Parser) {
     });
   };
 
+  /**
+   * Allows to add custom primitive parsers
+   */
+  var custom = Parsimmon.custom = function(parsingFunction) {
+    return Parser(function(stream, i) {
+      return parsingFunction(stream, i, makeSuccess, makeFailure);
+    });
+  };
+
   var alt = Parsimmon.alt = function() {
     var parsers = [].slice.call(arguments);
     var numParsers = parsers.length;
