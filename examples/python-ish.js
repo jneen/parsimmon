beb8a6e4d904ff2bc974d67363bfc153a3e7f1f8
Brian Mock <brian@mockbrian.com>
2017-07-08 13:43:25 -0700

    WIP

--- examples/python-ish.js
+++ examples/python-ish.js
@@ -7,14 +7,6 @@ let P = require('..');
 
 ///////////////////////////////////////////////////////////////////////
 
-let sneak = P((str, i, state) => {
-  console.log('\n\n\n----');
-  // console.log(JSON.stringify(str.slice(i)));
-  console.log(str.slice(i));
-  console.log(i, state);
-  return P.makeSuccess(i, null, state);
-});
-
 let Pythonish = P.createLanguage({
   Program: r =>
     r.Statement.many().trim(r._).node('Program'),
@@ -43,26 +35,48 @@ let Pythonish = P.createLanguage({
       return {statements};
     }).node('Block'),
 
+  // Uses standard Parsimmon indentation tracking. This includes either tabs or
+  // spaces, but does not support them mixed. Please note that Python actually
+  // allows mixed tabs and spaces, despite it being a not very good idea.
+  //
+  // https://docs.python.org/3/reference/lexical_analysis.html#indentation
   IndentMore: () => P.indentMore(P.countIndentation),
   IndentLess: () => P.indentLess(P.countIndentation),
   IndentSame: () => P.indentSame(P.countIndentation),
 
+  // Standard Python style comments
   Comment: r =>
     P.seq(
       P.string('#'),
       P.regexp(/[^\r\n]*/),
       r.End
     ),
+
+  // Zero or more blank lines; should be ignore for parsing purposes
   _: r => r.BlankLine.many(),
-  BlankLine: r => r.Spaces0.then(P.alt(r.Comment, r.NL)),
-  Terminator: r =>
-    r.Spaces0.then(P.alt(r.Comment, r.End)),
+
+  // A blank line which should be completely ignored for all parsing purposes
+  BlankLine: r => r.Spaces0.then(P.alt(r.Comment, r.Newline)),
+
+  // A logical "end" of a line can include spaces or a comment before the end
+  Terminator: r => r.Spaces0.then(P.alt(r.Comment, r.End)),
+
+  // Zero or more spaces or tabs
   Spaces0: () => P.regexp(/[ \t]*/),
-  CR: () => P.string('\r'),
-  LF: () => P.string('\n'),
-  CRLF: () => P.string('\r\n'),
-  NL: r => P.alt(r.CRLF, r.LF, r.CR).desc('newline'),
-  End: r => P.alt(r.NL, P.eof),
+
+  // Logical newlines can be:
+  //
+  // - Windows style ("\r\n" aka CRLF)
+  // - UNIX style ("\n" aka LF)
+  // - Mac OS 9 style ("\r" aka CR)
+  //
+  // Realistically, nobody uses Mac OS 9 style any more, but oh well.
+  Newline: () => P.alt(P.string('\r\n'), P.oneOf('\r\n')).desc('newline'),
+
+  // Typically text files *end* each line with a newline, rather than just
+  // separating them, but many files are malformed, so we should support the
+  // "end of file" as a form of newline.
+  End: r => P.alt(r.Newline, P.eof),
 });
 
 ///////////////////////////////////////////////////////////////////////
