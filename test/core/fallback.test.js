4a2ac88c50442df59292a9c8ed702dc4135ff9bb
minamorl <minamorl@users.noreply.github.com>
2017-08-19 17:41:41 +0900

    Make fallback be compatible with many()

--- test/core/fallback.test.js
+++ test/core/fallback.test.js
@@ -2,10 +2,21 @@
 
 suite('fallback', function() {
 
-  test('allows fallback result if no match is found', function() {
+  test('returns same result as original when match is found', function() {
     var parser = Parsimmon.string('a').fallback('nothing');
     assert.deepEqual(parser.parse('a').value, 'a');
+  });
+
+  test('allows fallback result if no match is found', function() {
+    var parser = Parsimmon.string('a').fallback('nothing');
     assert.deepEqual(parser.parse('').value, 'nothing');
   });
 
+  test('will work with many() method', function() {
+    var parser = Parsimmon.alt(
+      Parsimmon.string('b').fallback('nothing'),
+      Parsimmon.string('a')
+    ).many();
+    assert.deepEqual(parser.parse('bbba').value, ['b', 'b', 'b', 'nothing', 'a', 'nothing']);
+  });
 });
