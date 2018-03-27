715c5a7ead5ab4f01bbe12bcc6e6577b66075db8
Ivan Kleshnin <ivan@paqmind.com>
2018-03-27 09:59:05 +0300

    Restyle argv example a bit

--- examples/argv.js
+++ examples/argv.js
@@ -33,8 +33,10 @@ let CLI = P.createLanguage({
     // one of possible quotes, then sequence of anything except that quote (unless escaped), then the same quote
     return P.oneOf(`"'`).chain(function(q) {
       return P.alt(
-        P.noneOf(`\\${q}`).atLeast(1).tie(), // everything but quote and escape sign
-        P.string(`\\`).then(P.any)           // escape sequence like \"
+        P.noneOf(`\\${q}`)
+          .atLeast(1)
+          .tie(), // everything but quote and escape sign
+        P.string(`\\`).then(P.any) // escape sequence like \"
       )
         .many()
         .tie()
@@ -55,26 +57,46 @@ let ast = CLI.expression.tryParse(expression);
 prettyPrint(ast);
 
 /*
-Intentionally not supported:
+ #### Intentionally not supported
 
-  1) Space-delemited flag values
+ 1) Space-delemited flag values
 
-  ```
-  $ command --foo FOO
-  as
-  $ command --foo=FOO
-  ```
+ ```
+ $ command --foo FOO
+ as	+  as
+ $ command --foo=FOO
+ ```
 
-  because it's non-context-free. It's impossible to tell if `FOO` related to `command` or `--foo`
-  without an intimate knowledge of exact flags (which couples parser and application).
+ because it's non-context-free. It's impossible to tell if `FOO` related to `command` or `--foo`
+ without an intimate knowledge of exact flags (which couples parser and application).
 
-  2) Flag joining
+ 2) Flag joining
 
-  ```
-  $ command -fgh
-  as
-  $ command -f -g -h
-  ```
+ ```
+ $ command -fgh
+ as
+ $ command -f -g -h
+ ```
 
-  I just don't like this style. It's easy to confuse `-any` and `--any`.
+ I just don't like this style. It's easy to confuse `-any` and `--any`.
+
+ #### Nesting
+
+ Commands can be nested but the exact meaning of WORD is up to an application. For example:
+
+ ```
+ $ git pull origin master
+ ```
+
+ can be seen as one of:
+
+ ```
+ $ git arg1 arg2 arg3
+ or
+ $ git (pull arg1 arg2) -- you want this one
+ or
+ $ git (pull (origin arg))
+ ```
+
+ So the parser does what it does.
 */
