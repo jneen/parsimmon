e72c9af96814b73941dbf06907762dafc4b265c4
Brandon Keown <brandon.keown@taillight.com>
2018-04-05 09:05:57 -0500

    Add test to check contravariant composition property.

--- test/core/contramap.test.js
+++ test/core/contramap.test.js
@@ -13,4 +13,24 @@ suite("map", function() {
       Parsimmon.string("x").contramap("not a function");
     });
   });
+
+  test("upholds contravariant law of composition", function() {
+    function toLower(x) {
+      return x.toLowerCase();
+    }
+
+    function chrs(b) {
+      return b.toString("ascii");
+    }
+
+    var parser1 = Parsimmon.string("a")
+      .contramap(toLower)
+      .contramap(chrs);
+    var parser2 = Parsimmon.string("a").contramap(function(x) {
+      return toLower(chrs(x));
+    });
+    var input = Buffer.from([0x61]);
+
+    assert.deepEqual(parser1.parse(input), parser2.parse(input));
+  });
 });
