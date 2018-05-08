af81e471b944fcfb39bd4aef7799a885f432973a
Brian Mock <brian@mockbrian.com>
2018-05-08 11:37:37 -0700

    #191 adds newline-related default parsers

---
+++ test/core/misc.test.js
@@ -0,0 +1,13 @@
+// TODO: Test these miscellaneous parsers
+
+// var digit = regexp(/[0-9]/).desc("a digit");
+// var digits = regexp(/[0-9]*/).desc("optional digits");
+// var letter = regexp(/[a-z]/i).desc("a letter");
+// var letters = regexp(/[a-z]*/i).desc("optional letters");
+// var optWhitespace = regexp(/\s*/).desc("optional whitespace");
+// var whitespace = regexp(/\s+/).desc("whitespace");
+// var cr = string("\r");
+// var lf = string("\n");
+// var crlf = string("\r\n");
+// var nl = alt(crlf, lf, cr).desc("newline");
+// var end = alt(nl, eof);
