0577c93c6562a38e2b0d1b7c4291fea071bae071
Monroe Ekilah <monroe@ekilah.dev>
2020-08-24 18:08:08 -0700

    100% test coverage

--- test/core/flags.test.js
+++ test/core/flags.test.js
@@ -3,19 +3,33 @@
 testSetScenario(function() {
   describe("Parsimmon.flags()", function() {
     it("works in modern browsers", function() {
-      var flags = Parsimmon.flags(/a/gim);
-      assert.strictEqual(flags, "gim");
+      // eslint-disable-next-line no-invalid-regexp
+      var flags = Parsimmon.flags(new RegExp("a", "gimuy"));
+      assert.strictEqual(flags, "gimuy");
     });
 
-    it("works on legacy browsers without Regexp.flags property", function() {
-      var oldRegExp = /a/gim;
+    it("works on legacy browsers without Regexp.flags property with flags", function() {
+      // eslint-disable-next-line no-invalid-regexp
+      var oldRegExp = new RegExp("a", "gimuy");
 
       // Simulate old RegExp without the flags property
       Object.defineProperty(oldRegExp, "flags", { value: undefined });
       assert.strictEqual(oldRegExp.flags, undefined);
 
       var flags = Parsimmon.flags(oldRegExp);
-      assert.strictEqual(flags, "gim");
+      assert.strictEqual(flags, "gimuy");
+    });
+
+    it("works on legacy browsers without Regexp.flags property without flags", function() {
+      // eslint-disable-next-line no-invalid-regexp
+      var oldRegExp = new RegExp("a", "");
+
+      // Simulate old RegExp without the flags property
+      Object.defineProperty(oldRegExp, "flags", { value: undefined });
+      assert.strictEqual(oldRegExp.flags, undefined);
+
+      var flags = Parsimmon.flags(oldRegExp);
+      assert.strictEqual(flags, "");
     });
   });
 });
