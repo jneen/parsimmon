d07bcb7a5567a4183cfdae8afc2e52aff1f47159
Monroe Ekilah <monroe@ekilah.dev>
2020-08-24 18:14:37 -0700

    remove eslint-ignore

--- test/core/flags.test.js
+++ test/core/flags.test.js
@@ -3,13 +3,11 @@
 testSetScenario(function() {
   describe("Parsimmon.flags()", function() {
     it("works in modern browsers", function() {
-      // eslint-disable-next-line no-invalid-regexp
       var flags = Parsimmon.flags(new RegExp("a", "gimuy"));
       assert.strictEqual(flags, "gimuy");
     });
 
     it("works on legacy browsers without Regexp.flags property with flags", function() {
-      // eslint-disable-next-line no-invalid-regexp
       var oldRegExp = new RegExp("a", "gimuy");
 
       // Simulate old RegExp without the flags property
@@ -21,7 +19,6 @@ testSetScenario(function() {
     });
 
     it("works on legacy browsers without Regexp.flags property without flags", function() {
-      // eslint-disable-next-line no-invalid-regexp
       var oldRegExp = new RegExp("a", "");
 
       // Simulate old RegExp without the flags property
